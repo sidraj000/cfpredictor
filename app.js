59920079208f7c5886217d7240d21cbab0d0d863
sid2104 <you@example.com>
2020-03-30 20:51:07 +0530

    done

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const https = require('https');
 const express= require('express');
 const app=express();
-refreshData();
 function currtime()
 {
   let ts = Date.now();
@@ -9,35 +8,35 @@ function currtime()
   var unixTimestamp = Math.round(currDate.getTime()/1000);
   return unixTimestamp;
 }
-// function sendmail(mailDetail)
-// {
-//   const nodemailer = require('nodemailer'); 
+function sendmail(mailDetail)
+{
+  const nodemailer = require('nodemailer'); 
   
   
-// let mailTransporter = nodemailer.createTransport({ 
-//     service: 'gmail', 
-//     auth: { 
-//         user: 'siddharthraj.civ18@itbhu.ac.in', 
-//         pass: 'siddhuraj000@'
-//     } 
-// }); 
+let mailTransporter = nodemailer.createTransport({ 
+    service: 'gmail', 
+    auth: { 
+        user: 'siddharthraj.civ18@itbhu.ac.in', 
+        pass: 'jqiyoyvlmhihkfnp'
+    } 
+}); 
   
-// // let mailDetails = { 
-// //     from: 'siddharthraj.civ18@itbhu.ac.in', 
-// //     to: 'siddharthraj000@gmail.com', 
-// //     subject: 'Test mail', 
-// //     text: 'Node.js testing mail for GeeksforGeeks'
-// // }; 
-// let mailDetails=mailDetail;
+// let mailDetails = { 
+//     from: 'siddharthraj.civ18@itbhu.ac.in', 
+//     to: 'siddharthraj000@gmail.com', 
+//     subject: 'Test mail', 
+//     text: 'Node.js testing mail for GeeksforGeeks'
+// }; 
+let mailDetails=mailDetail;
   
-// mailTransporter.sendMail(mailDetails, function(err, data) { 
-//     if(err) { 
-//         console.log('Error Occurs'); 
-//     } else { 
-//         console.log('Email sent successfully'); 
-//     } 
-// }); 
-// }
+mailTransporter.sendMail(mailDetails, function(err, data) { 
+    if(err) { 
+        console.log(err.message); 
+    } else { 
+        console.log('Email sent successfully'); 
+    } 
+}); 
+}
 function refreshData()
 {
   https.get('https://codeforces.com/api/user.status?handle=sid2104&from=1&count=10', (resp) => {
@@ -53,32 +52,41 @@ function refreshData()
     });
     resp.on('end', () => {
       var jsres=(JSON.parse(data));
-      // for(i in jsres.result)
-      // {
-       var name=jsres.result[0].problem.name;
-    var verdict=jsres.result[0].verdict;
-    var seconds=jsres.result[0].creationTimeSeconds;
+      for(i in jsres.result)
+      {
+       var name=jsres.result[i].problem.name;
+    var verdict=jsres.result[i].verdict;
+    var seconds=jsres.result[i].creationTimeSeconds;
      date=new Date(seconds*1000);
-       console.log(name+" "+verdict+" "+date+" ");
-  let mailDetails={
-    from: 'siddharthraj.civ18@itbhu.ac.in', 
-      to: 'siddharthraj000@gmail.com', 
-      subject: 'Test mail', 
-      text: name+"  "+verdict};
-    //  var x=jsres.result[i];
-   
-    //   console.log(x);
-      
+     //  console.log(name+" "+verdict+" "+date+" ");
+     // console.log(seconds);
+     if(unixTimestamp-seconds<=35)
+     {
+       console.log("YES");
+      let mailDetail={
+        from: 'siddharthraj.civ18@itbhu.ac.in', 
+          to: 'siddharthraj000@gmail.con', 
+          subject: 'Mail from siddharth', 
+          text: name+"  "+verdict};
+       sendmail(mailDetail); 
+     }
+     else
+     {
+       console.log(unixTimestamp-seconds);
+     }
+  
+
+  }
     });
-   // sendmail(mailDetails);
+   
   
   }
   ).on("error", (err) => {
     console.log("Error: " + err.message);
   });
- // setTimeout(refreshData,30*1000);
+ setTimeout(refreshData,30*1000);
 }
-//refreshData();
+refreshData();
 
 
 
